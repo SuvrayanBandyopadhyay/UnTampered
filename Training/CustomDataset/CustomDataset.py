c8c4e0711a4e3f6c386b669bc105cc752a86ba2f
Suvrayan Bandyopadhyay <suvrayanb@gmail.com>
2026-03-22 16:14:36 +0530

    [ADD] Added basic classifier model

--- Training/CustomDataset/CustomDataset.py
+++ Training/CustomDataset/CustomDataset.py
@@ -1,7 +1,7 @@
 import torchvision.transforms as transforms
 import tqdm
 import os
-from PIL import Image,ImageChops,ImageEnhance
+from PIL import Image,ImageChops,ImageEnhance,ImageFilter
 from torch.utils.data import Dataset
 import torch
 import io
@@ -29,6 +29,19 @@ def convert_to_ela_image(image,quality=90):
     ela_image = ImageEnhance.Brightness(ela_image).enhance(scale)
     return ela_image
 
+def convert_to_noisemap(image):
+    denoised_image = image.filter(ImageFilter.MedianFilter(size=3))
+    noise_map = ImageChops.difference(image,denoised_image)
+
+    extrema = noise_map.getextrema()
+    max_diff = sum([ex[1] for ex in extrema])/3
+    if(max_diff == 0):
+        max_diff = 1
+    
+    scale = 255.0/max_diff
+    noise_map = ImageEnhance.Brightness(noise_map).enhance(scale)
+
+    return noise_map
 
 class CustomDataset(Dataset): 
     def __init__(self,orig_path,mask_path,tampered_path,n=128): 
@@ -36,27 +49,35 @@ class CustomDataset(Dataset):
         transform = transforms.Compose([transforms.Resize((n,n)),transforms.ToTensor()]) 
         #Define the containers 
         self.data = [] 
-        mask = {} 
         #Since there is a many to one mapping 
-        # #Load the images 
-        # #Original 
+        # Load the images 
+        # Original 
         for f in tqdm.tqdm(os.listdir(orig_path)): 
             p = os.path.join(orig_path,f) 
             img = Image.open(p).convert("RGB") 
             ela_img = convert_to_ela_image(img)
+            noise_img = convert_to_noisemap(img)
+
             image_tensor = transform(img) 
             ela_tensor = transform(ela_img)
-            image_tensor = torch.cat([image_tensor,ela_tensor],dim=0)
+            noise_tensor = transform(noise_img)
+
+            image_tensor = torch.cat([image_tensor,ela_tensor,noise_tensor],dim=0)
             zero_mask = torch.zeros(1,n, n) 
-            self.data.append((image_tensor,zero_mask)) 
+            label = torch.tensor(0,dtype=torch.float32)
+            self.data.append((image_tensor,zero_mask,label)) 
 
         for f in tqdm.tqdm(os.listdir(tampered_path)):
             p = os.path.join(tampered_path, f)
             img = Image.open(p).convert("RGB")
             ela_img = convert_to_ela_image(img)
+            noise_img = convert_to_noisemap(img)
+
             image_tensor = transform(img)
             ela_tensor = transform(ela_img)
-            image_tensor = torch.cat([image_tensor, ela_tensor], dim=0)
+            noise_tensor = transform(noise_img)
+            
+            image_tensor = torch.cat([image_tensor, ela_tensor,noise_tensor], dim=0)
 
             name = os.path.splitext(f)[0]
             mask_path_full = None
@@ -79,8 +100,8 @@ class CustomDataset(Dataset):
             )(mask_img)
             mask_tensor = torch.from_numpy(np.array(mask_img)) / 255.0
             mask_tensor = mask_tensor.unsqueeze(0)
-
-            self.data.append((image_tensor, mask_tensor))
+            label = torch.tensor(1,dtype=torch.float32)
+            self.data.append((image_tensor, mask_tensor,label))
             
     def __len__(self): 
         return len(self.data) 
